9f80e1837daa5fdaba6014903f666c3968892efd
jianghu <jianghu2333@163.com>
2019-06-27 11:40:36 +0800

    添加自定义级别日志

--- src/main/java/log4j/demo/App.java
+++ src/main/java/log4j/demo/App.java
@@ -3,14 +3,19 @@ package log4j.demo;
 
 import org.apache.log4j.Logger;
 
+import java.util.Date;
+
 /**
  * Hello world!
  */
 public class App {
 
     public static void main(String[] args) {
-        Logger logger = Logger.getLogger(App.class);
-        logger.info("sssssssssssssssssssss");
-        logger.error("这是错误");
+        Logger logger = CustomLog.getInstance().createLogger("d:/logggg/", "wd_error", "--->"+new Date(), true);
+        CustomLog.getInstance().customLog(logger,"测试");
+        CustomLog.getInstance().customLog(logger,"222222222222");
+        CustomLog.getInstance().closeCustomLog(logger);
+
+
     }
 }
